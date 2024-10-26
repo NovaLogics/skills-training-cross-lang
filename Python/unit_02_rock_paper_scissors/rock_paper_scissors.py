3038a7cdc7fdfe86ee7f58423deeeba0698a802b
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-26 15:42:10 +0530

    feat [ python ] : Add : Rock, Paper, Scissors program

---
+++ Python/unit_02_rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,5 @@
+# Here is a online Python compiler (interpreter) to run Python online.
+# https://www.programiz.com/python-programming/online-compiler/
+# -------------------------------------------------------------------
+# Unit 02 : Rock, Paper, Scissors
+# -------------------------------------------------------------------
